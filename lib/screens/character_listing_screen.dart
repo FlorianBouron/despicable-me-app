6011d92a73f486def82fc2042c6afc71e4baeacc
Florian Bouron <bouron.florian@yahoo.fr>
2020-02-12 21:15:54 +0100

    Add Character Listing screen

---
+++ lib/screens/character_listing_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/character.dart';
+import '../style.dart';
+
+class CharacterListingScreen extends StatefulWidget {
+  @override
+  _CharacterListingScreenState createState() => _CharacterListingScreenState();
+}
+
+class _CharacterListingScreenState extends State<CharacterListingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.arrow_back_ios),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 32.0,
+                  top: 8.0,
+                ),
+                child: RichText(
+                  text: TextSpan(
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: "Despicable Me",
+                        style: AppTheme.title,
+                      ),
+                      TextSpan(text: "\n"),
+                      TextSpan(
+                        text: "Characters",
+                        style: AppTheme.subTitle,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Character(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
