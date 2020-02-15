09a3407bb1f52b1f45d0b9bf0e5ccedba82dcc2f
Florian Bouron <bouron.florian@yahoo.fr>
2020-02-16 00:02:32 +0100

    Add Clips and animation to character details

--- lib/screens/character_details_sreen.dart
+++ lib/screens/character_details_sreen.dart
@@ -4,6 +4,9 @@ import '../models/character.dart';
 import '../style.dart';
 
 class CharacterDetailsScreen extends StatefulWidget {
+  final double _expandedBottomSheetBottom = 0;
+  final double _collapsedBottomSheetBottom = -250;
+
   final Character character;
 
   const CharacterDetailsScreen({Key key, this.character}) : super(key: key);
@@ -13,6 +16,20 @@ class CharacterDetailsScreen extends StatefulWidget {
 }
 
 class _CharacterDetailsScreenState extends State<CharacterDetailsScreen> {
+  double _bottomSheetBottomPosition = -330;
+  bool isCollapsed = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        isCollapsed = true;
+        _bottomSheetBottomPosition = widget._collapsedBottomSheetBottom;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
@@ -94,8 +111,111 @@ class _CharacterDetailsScreenState extends State<CharacterDetailsScreen> {
               ],
             ),
           ),
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.decelerate,
+            bottom: _bottomSheetBottomPosition,
+            left: 0,
+            right: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  InkWell(
+                    onTap: _onTap,
+                    child: Container(
+                      alignment: Alignment.centerLeft,
+                      padding: const EdgeInsets.symmetric(horizontal: 32),
+                      height: 80,
+                      child: Text(
+                        "Clips",
+                        style: AppTheme.subHeading.copyWith(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: _clipsWidget(),
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
+  }
+
+  Widget _clipsWidget() {
+    return Container(
+      height: 250,
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              roundedContainer(Colors.redAccent),
+              SizedBox(height: 20),
+              roundedContainer(Colors.greenAccent),
+            ],
+          ),
+          SizedBox(width: 16),
+          Column(
+            children: <Widget>[
+              roundedContainer(Colors.orangeAccent),
+              SizedBox(height: 20),
+              roundedContainer(Colors.purple),
+            ],
+          ),
+          SizedBox(width: 16),
+          Column(
+            children: <Widget>[
+              roundedContainer(Colors.grey),
+              SizedBox(height: 20),
+              roundedContainer(Colors.blueGrey),
+            ],
+          ),
+          SizedBox(width: 16),
+          Column(
+            children: <Widget>[
+              roundedContainer(Colors.lightGreenAccent),
+              SizedBox(height: 20),
+              roundedContainer(Colors.pinkAccent),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget roundedContainer(Color color) {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.all(
+          Radius.circular(20),
+        ),
+      ),
+    );
+  }
+
+  _onTap() {
+    setState(() {
+      _bottomSheetBottomPosition = isCollapsed
+          ? widget._expandedBottomSheetBottom
+          : widget._collapsedBottomSheetBottom;
+      isCollapsed = !isCollapsed;
+    });
   }
 }
